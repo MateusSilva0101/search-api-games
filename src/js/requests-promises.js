9ff8ab89f4f15b45aa2c1686271a430bc58b985b
MateusSilva0101 <mateusrodrigues.suporteti01@gmail.com>
2022-05-29 17:17:18 -0300

    alterações na sintaxe do javascript adicionando arrow function

--- src/js/requests-promises.js
+++ src/js/requests-promises.js
@@ -1,10 +1,8 @@
-function getGamesByName (name, callback) {  
+const  getGamesByName = (name, callback) => {  
     let request = new XMLHttpRequest();
-    request.open('GET', `https://api.rawg.io/api/games?key=${CHAVE_API}&search=${name}`, true);
-    //console.log(request);
+    request.open('GET', `https://api.rawg.io/api/games?key=${chaveapi}&search=${name}`, true);
     request.onload = () => {
         let response = JSON.parse(request.response);
-        //console.log(response);
         if(typeof callback == 'function') callback(response);
     };
     request.send();
